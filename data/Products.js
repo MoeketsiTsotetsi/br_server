d7a138c9f2cc41848402a3a11077a51cfabf89bf
Moeketsi Tsotetsi <tmoeketsid@gmail.com>
2023-01-06 23:58:32 +0200

    Protect issues persits

---
+++ data/Products.js
@@ -0,0 +1,64 @@
+const products = [
+  {
+    name: "Dining Room Set",
+    image: "/images/dining.png",
+    description:
+      "Dining set consisting of multiple seats",
+    price: 4000,
+    countInStock: 3,
+    rating: 4,
+    numReviews: 4,
+  },
+  {
+    name: "Black Wooden Kitchen Set",
+    image: "/images/Kitchen.png",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galleyof type and scrambled it to make a type specimen book",
+    price: 2500,
+    countInStock: 10,
+    rating: 2,
+    numReviews: 2,
+  },
+  {
+    name: "Outdoor Table Set",
+    image: "/images/Outdoor.png",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book",
+    price: 1600,
+    countInStock: 0,
+    rating: 3.5,
+    numReviews: 3,
+  },
+  {
+    name: "Wooden Cupboard",
+    image: "/images/cupboards.png",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book",
+    price: 3500,
+    countInStock: 10,
+    rating: 5,
+    numReviews: 9,
+  },
+  {
+    name: "Modern Cafe Set",
+    image: "/images/mordern_cafe.png",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing andtypesetting industry. Lorem Ipsum has been the industry's standarddummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book",
+    price: 6500,
+    countInStock: 7,
+    rating: 2,
+    numReviews: 2,
+  },
+  {
+    name: "Office Table",
+    image: "/images/office.png",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing andtypesetting industry. Lorem Ipsum has been the industry's standarddummy text ever since the 1500s, when an unknown printer took a galleyof type and scrambled it to make a type specimen book",
+    price: 800,
+    countInStock: 1,
+    rating: 0,
+    numReviews: 0,
+  },
+];
+
+export default products;
